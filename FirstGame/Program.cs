e634b2df9b08be1e812bef572c9ce70bae435a4f
Arz80 <1980sam2006@gmail.com>
2023-02-19 19:18:55 +0600

    Добавили логику в Program

--- FirstGame/Program.cs
+++ FirstGame/Program.cs
@@ -2,8 +2,44 @@
 
 class Program
 {
+    private static Game _game = new Game();
     static void Main(string[] args)
     {
-        
+        while (_game.GetWinner() == Winner.None)
+        {
+            Console.Clear();
+            PrintGame();
+            Console.Write("Введите букву: ");
+            char res = Console.ReadKey().KeyChar;
+            _game.TurnLetter(res);
+
+
+        }
+
+        Console.Clear();
+        PrintGame();
+        if (_game.GetWinner() == Winner.Player)
+        {
+            Console.WriteLine("Победил Игрок");
+        }
+        else
+        {
+            Console.WriteLine($"Слово было - {_game.Word}");
+            Console.WriteLine("Победил Компьютер");
+        }
+
+        Console.ReadKey();
+    }
+
+    static void PrintGame()
+    {
+        Console.WriteLine("Виселица");
+        Console.WriteLine();
+        Console.Write("Введите букву из загаданного слова. ");
+        Console.WriteLine($"У вас осталось {_game.count} попыток");
+        Console.WriteLine();
+        Console.WriteLine($"{_game.answerWord}");
+
+
     }
 }
